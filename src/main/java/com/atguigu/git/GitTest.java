d69e0d16aa4057f602bad5fcaa00cd56b33eab32
lishier <2958835064@qq.com>
2023-01-31 16:43:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shier
+ * @create 2023-01-31 16:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
